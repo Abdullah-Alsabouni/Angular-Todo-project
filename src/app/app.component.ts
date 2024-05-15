6430b472ec3e01d3f45e4543c40c62017e0bffbd
Abdullah Alsabouni <21061149@stu.omu.edu.tr>
2024-05-15 16:13:09 +0300

    Lesson 6-Display All

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,17 @@ import { Model, TodoItem } from './Model';
 })
 export class AppComponent {
   model1 = new Model();
+  isDisplay = false; // Default olarak işaretli olmasın
   
   getName() {
     return this.model1.user;
   }
 
   getItems() {
+    if(this.isDisplay){ // eğer isDisplay = false ise Tüm itemleri getir
+      return this.model1.İtems;
+    }
+
     return this.model1.İtems.filter(item => !item.action); // filtreleme yap. Sadece false olanları listede bırak
   }
   addItem(value: string){
